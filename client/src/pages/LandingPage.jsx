386c58fe1fd8689317183cad153ae7b77e49c863
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-07-25 01:38:07 +0700

    Adding Style for full screen component and add background

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Header from "../components/Header";
+import FooterHeader from "../components/FooterHeader";
+import { makeStyles } from "@material-ui/core/styles";
+import Background from "../assets/img/bg1.png";
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    marginTop: 0,
+    height: "100vh",
+    backgroundImage: `url(${Background})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+}));
 
 export default function LandingPage() {
+  const classes = useStyles();
   return (
-    <div className="app">
-      <Header />
-      <h1>Landing Page</h1>
+    <div className={classes.section}>
+      <FooterHeader />
     </div>
   );
 }
